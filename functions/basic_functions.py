725e1412129fb86afa10ba2076c63b97a587b035
SerikbaevZhubanysh <serikbaevzhubanysh2007gmail.com>
2026-02-10 18:20:49 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ functions/basic_functions.py
@@ -0,0 +1,21 @@
+# 1
+def my_function():
+  print("Hello from a function")
+
+# 2def my_function():
+  print("Hello from a function")
+
+my_function()
+
+# 3
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+# 4
+def greet():
+    print("Hello, welcome!")
+
